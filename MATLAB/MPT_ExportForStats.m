1d23057b523e1ca3318fba01e5cb94dfd8a960cb
Yannick Roy <ymyannick@gmail.com>
2016-05-24 21:35:27 -0400

    Automate Export added. (all measures, all domains)

--- MATLAB/MPT_ExportForStats.m
+++ MATLAB/MPT_ExportForStats.m
@@ -1,6 +1,8 @@
 % YR 2015 %
 % Script to export MPT's data for stats in R (or else).
 
+%% Manual Export
+
 %=============================
 % Nima's Code to export Data.
 %=============================
@@ -21,3 +23,42 @@ freqs = STUDY.measureProjection.ersp.object.frequency;
 % Export data.
 save('export_test_mpt', 'linearProjectedMeasure', 'sessionConditionCell', 'groupId', 'uniqeDatasetId', 'dipoleDensity', 'times', 'freqs');
 %save('export_mpt', 'linearProjectedMeasure', 'sessionConditionCell', 'groupId', 'uniqeDatasetId', 'dipoleDensity', 'times', 'freqs');
+
+
+%% Automatic Export
+measures = {'erp', 'ersp'};
+
+for m = measures
+    curMeasure = m{1};
+    
+    disp(curMeasure);
+    if strcmp(curMeasure, 'ersp')
+        % ERSPs Time/Freq Axes.
+        times = STUDY.measureProjection.ersp.object.time;
+        freqs = STUDY.measureProjection.ersp.object.frequency;
+    end
+    
+    for curDomain = 1:length(STUDY.measureProjection.(curMeasure).projection.domain)
+        disp(curDomain);
+        
+        %=============================
+        % Nima's Code to export Data.
+        %=============================
+        domainNumber = curDomain;
+        dipoleAndMeasure = STUDY.measureProjection.(curMeasure).object; % get the ERSP and dipole data (dataAndMeasure object) from the STUDY structure.
+        domain = STUDY.measureProjection.(curMeasure).projection.domain(domainNumber); % get the domain in a separate variable
+        projection  = STUDY.measureProjection.(curMeasure).projection;
+        headGrid = STUDY.measureProjection.(curMeasure).headGrid;
+        [linearProjectedMeasure sessionConditionCell groupId uniqeDatasetId dipoleDensity] = dipoleAndMeasure.getMeanProjectedMeasureForEachSession(headGrid, domain.membershipCube, projection.projectionParameter);
+        %=============================
+        % Nima's Code to export Data.
+        %=============================
+
+        % Export data.
+        if strcmp(curMeasure, 'ersp')
+            save(['MPT_exp_' curMeasure '_D' num2str(curDomain)], 'linearProjectedMeasure', 'sessionConditionCell', 'groupId', 'uniqeDatasetId', 'dipoleDensity', 'times', 'freqs');
+        else
+            save(['MPT_exp_' curMeasure '_D' num2str(curDomain)], 'linearProjectedMeasure', 'sessionConditionCell', 'groupId', 'uniqeDatasetId', 'dipoleDensity');
+        end   
+    end
+end
